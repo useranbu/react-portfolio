5c7b64f4d8bdf0c6e15d79cbf25c58408cd9b34c
RadhaKrishnan-D <118905403+RadhaKrishnan-D@users.noreply.github.com>
2022-11-24 12:04:09 +0530

    Update Nav.jsx

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,53 +1,93 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Nav.css";
-import {
-  AiOutlineHome,
-  AiOutlineUser,
-  AiFillSignal,
-  AiOutlineIdcard,
-} from "react-icons/ai";
-import { BsFillChatQuoteFill } from "react-icons/bs";
-import { useState } from "react";
 
-const Nav = () => {
-  const [activeNav, setActiveNav] = useState("#");
+const Nav = ({ navItems }) => {
+  const [activeNav, setActiveNav] = useState(`#${navItems[0].navID}`);
+
+  const navHeader = navItems;
+
+  useEffect(() => {
+    const handleScroll = (e) => {
+      const index = nearestIndex(
+        window.scrollY,
+        navHeader,
+        0,
+        navHeader.length - 1
+      );
+      const id = `#${navItems[index].navID}`
+      setActiveNav(id);
+    };
+    document.addEventListener("scroll", handleScroll);
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const nearestIndex = (
+    currentPosition,
+    sectionPositionArray,
+    startIndex,
+    endIndex
+  ) => {
+    if (startIndex === endIndex) return startIndex;
+    else if (startIndex === endIndex - 1) {
+      if (
+        Math.abs(
+          sectionPositionArray[startIndex].navRef.current?.offsetTop -
+          currentPosition
+        ) <
+        Math.abs(
+          sectionPositionArray[endIndex].navRef.current?.offsetTop -
+          currentPosition
+        )
+      )
+        return startIndex;
+      else return endIndex;
+    } else {
+      let nextNearest = ~~((startIndex + endIndex) / 2);
+      let a = Math.abs(
+        sectionPositionArray[nextNearest].navRef.current?.offsetTop -
+        currentPosition
+      );
+      let b = Math.abs(
+        sectionPositionArray[nextNearest + 1].navRef.current?.offsetTop -
+        currentPosition
+      );
+      if (a < b) {
+        return nearestIndex(
+          currentPosition,
+          sectionPositionArray,
+          startIndex,
+          nextNearest
+        );
+      } else {
+        return nearestIndex(
+          currentPosition,
+          sectionPositionArray,
+          nextNearest,
+          endIndex
+        );
+      }
+    }
+  };
+
   return (
     <nav>
-      <a
-        href="#home"
-        onClick={() => setActiveNav("#")}
-        className={activeNav === "#" ? "active" : ""}
-      >
-        <AiOutlineHome />
-      </a>
-      <a
-        href="#about"
-        onClick={() => setActiveNav("#about")}
-        className={activeNav === "#about" ? "active" : ""}
-      >
-        <AiOutlineUser />
-      </a>
-      <a
-        href="#experiance"
-        onClick={() => setActiveNav("#experiance")}
-        className={activeNav === "#experiance" ? "active" : ""}
-      >
-        <AiFillSignal />
-      </a>
-      <a
-        href="#testimonials"
-        onClick={() => setActiveNav("#testimonials")}
-        className={activeNav === "#testimonials" ? "active" : ""}
-      >
-        <BsFillChatQuoteFill />
-      </a>
-      <a
-        href="#contact"
-        onClick={() => setActiveNav("#contact")}
-        className={activeNav === "#contact" ? "active" : ""}
-      >
-        <AiOutlineIdcard />
-      </a>
+      {
+        navItems.map(value => {
+          const Component = value.navIcon;
+          return (
+            <a
+              key={`#${value.navID}`}
+              href={`#${value.navID}`}
+              onClick={() => setActiveNav(`#${value.navID}`)}
+              className={activeNav === `#${value.navID}` ? "active" : ""}
+            >
+              <Component />
+            </a>
+          )
+        })
+      }
     </nav>
   );
 };
